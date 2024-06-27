074acf48cdd76ce9c7ce3a37cdb8483d0b0de48a
Rafael <rafael.pacco@gmail.com>
2024-06-27 16:07:26 -0300

    feat: checking date bf scraping data

--- abicom/spiders/ppi_crawler.py
+++ abicom/spiders/ppi_crawler.py
@@ -4,20 +4,30 @@ from datetime import datetime
 import pandas as pd
 from abicom.items import PpiItem
 
-
 class PpiCrawlerSpider(scrapy.Spider):
     name = "ppi_crawler"
     allowed_domains = ["abicom.com.br"]
-    # start_urls = ["https://abicom.com.br/categoria/ppi/page/1"]
+    start_urls = ["https://abicom.com.br/categoria/ppi/page/1"]
     today = datetime.today().date()
 
-    date_range = pd.date_range(end=today, periods=520, freq='B').strftime('%d-%m-%Y')
-
-    url = 'https://abicom.com.br/ppi/ppi-{}/'
-
     def start_requests(self) -> Iterable[scrapy.Request]:
-        for date in self.date_range:
-            yield scrapy.Request(self.url.format(date), meta={'date': date})
+        url_date = 'https://abicom.com.br/categoria/ppi/'
+        yield scrapy.Request(url_date, callback=self.check_date)
+
+    def check_date(self, response):
+        last_date_text = response.css('div.card-date::text').get()
+        last_date = pd.to_datetime(last_date_text, dayfirst=True).date()
+
+        if last_date == self.today:
+            self.log("Latest date matches today's date, continuing the script.")
+            date_range = pd.date_range(end=self.today, periods=520, freq='B').strftime('%d-%m-%Y')
+            url = 'https://abicom.com.br/ppi/ppi-{}/'
+
+            for date in date_range:
+                yield scrapy.Request(url.format(date), meta={'date': date}, callback=self.parse)
+        else:
+            self.log("Latest date does not match today's date, stopping the spider.")
+            return
 
     def parse(self, response):
         date = response.meta['date']
