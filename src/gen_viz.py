fdec7ac2d0041ab2af40f4b99362c4c42930a1b2
Rafael <rafael.pacco@gmail.com>
2024-08-23 18:15:19 -0300

    update: improved chart viz

--- src/gen_viz.py
+++ src/gen_viz.py
@@ -5,7 +5,7 @@ import matplotlib.dates as mdates
 from matplotlib.ticker import FuncFormatter
 from datetime import datetime
 import json
-import adjustText
+from adjustText import adjust_text
 
 # Formatting y-axis labels
 def currency_formatter(x, pos):
@@ -53,39 +53,62 @@ def gen_graph(df, combustivel):
 
     today = datetime.today().date()
     plt.figure(figsize=(10, 5))
-    plt.gca().set_facecolor('#f2f2f2')  # Set background color to light gray
 
-    ax = sns.lineplot(data=df, y=f'{combustivel}', x=df.index, color='black', linewidth=2, label=f'{label_comb}')
-    sns.lineplot(data=df, y='year_ma', x=df.index, color='goldenrod', linewidth=1, label='Média móvel 252 dias')
+    ax = sns.lineplot(data=df, y=f'{combustivel}', x=df.index, color='blue', linewidth=2, label=f'Defasagem {label_comb.lower()}')
+    sns.lineplot(data=df, y='year_ma', x=df.index, color='green' if df['year_ma'].iloc[-1] >=0 else 'salmon', linewidth=1.5, label='Média móvel 252 dias')
 
     date_format = mdates.DateFormatter('%b-%Y')
     plt.gca().xaxis.set_major_formatter(date_format)
     plt.gca().xaxis.set_major_locator(mdates.MonthLocator())
     plt.xticks(rotation=90)
     plt.xlabel('')
-    plt.hlines(y=0, xmin=df.index[0], xmax=df.index[-1], linestyles=(5, (10,3)), alpha=0.8, colors='gray')
+    plt.hlines(y=0, xmin=df.index[0], xmax=df.index[-1], linestyles='dashed', alpha=0.2)
 
     ax.set_title(f'Defasagem média {label_comb} nos principais polos Petrobrás\n (R$/L)', weight='bold')
     plt.figtext(0.02, 0.95, 'Fonte: Abicom', fontsize=8, color='gray')
 
-    ax.yaxis.set_major_formatter(FuncFormatter(currency_formatter))
+    def custom_formatter(x, pos):
+        return f"-R${abs(x):,.2f}" if x < 0 else f"R${x:,.2f}"
+    ax.yaxis.set_major_formatter(FuncFormatter(custom_formatter))
     plt.ylabel('')
 
     ax.spines['top'].set_visible(False)
     ax.spines['right'].set_visible(False)
     ax.spines['left'].set_visible(False)
 
-    texts = []
+    last_comb_value = ax.annotate(
+        f'{df[f"{combustivel}"].values[-1]:.2f}', 
+        (df[f"{combustivel}"].index[-1], df[f"{combustivel}"].values[-1]), 
+        # textcoords="offset points",
+        # xytext=(5,0), 
+        # ha='left', 
+        fontsize=10, 
+        color='black', 
+        weight='bold'
+    )
+    last_ma_value = ax.annotate(
+        f'{df["year_ma"].values[-1]:.2f}', 
+        (df["year_ma"].index[-1], 
+        df["year_ma"].values[-1]), 
+        # textcoords="offset points", 
+        # xytext=(5,0), 
+        # ha='left', 
+        fontsize=10, 
+        color='green' if df["year_ma"].values[-1] > 0 else 'red', 
+        weight='bold'
+        )
+    
+    adjust_text([last_comb_value, last_ma_value], only_move={'text': 'y'})
+    # Get the current x-coordinates of the annotations
+    x_comb = last_comb_value.get_position()[0]
+    x_ma = last_ma_value.get_position()[0]
 
-    # Annotate the last value of 'combustivel' with the same color as the line plot
-    texts.append(ax.annotate(f"{df[combustivel].values[-1]:.2f}", 
-                (df.index[-1], df[combustivel].values[-1]), 
-                textcoords="offset points", xytext=(5, 0), ha='left', fontsize=10, color='black', weight='bold'))
+    # Calculate the offset
+    x_offset = 2  # in data units, you might need to adjust this value
 
-    # Annotate the last value of 'year_ma' with the same color as the line plot
-    texts.append(ax.annotate(f"{df['year_ma'].values[-1]:.2f}", 
-                (df.index[-1], df['year_ma'].values[-1]), 
-                textcoords="offset points", xytext=(5, 0), ha='left', fontsize=10, color='goldenrod', weight='bold'))
+    # Set the new x-coordinates
+    last_comb_value.set_position((x_comb + x_offset, last_comb_value.get_position()[1]))
+    last_ma_value.set_position((x_ma + x_offset, last_ma_value.get_position()[1]))
 
     # Annotate the smallest values
     for value, date in zip(nsmallest.values, nsmallest.index):
@@ -97,14 +120,6 @@ def gen_graph(df, combustivel):
 
     plt.tick_params(axis='both', length=0, width=0)
     ax.spines['bottom'].set_color('gray')
-    # Add horizontal gridlines
-    plt.grid(axis='y', linestyle='--', alpha=0.5, color='gray')
-
-    # Adjust the annotations to avoid overlap
-    adjustText.adjust_text(texts, 
-                force_explode=(0.1, 0.1),  # expand only in y-direction more than x-direction
-                only_move={'points': 'y', 'texts': 'y'}
-            )
 
     plt.tight_layout()
     plt.legend()
