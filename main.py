f444511e1d5f4c8fa9ebbd79a96434b33bd925fd
Rafael <rafael.pacco@gmail.com>
2024-08-27 10:27:55 -0300

    Revert "feat: fixed data path dir"
    This reverts commit c612b3540b93e907d5a63aa346670a914304bfac.

--- main.py
+++ main.py
@@ -1,30 +1,13 @@
 from src.gen_viz import *
 from src.tweet import *
-from datetime import datetime, timedelta
+from datetime import datetime
 import subprocess
 import sys
-import os
-
-
-# Get the current working directory
-current_dir = os.path.dirname(os.path.abspath(__file__))
-
-# Construct the absolute path for the output files
-data_dir = os.path.join(current_dir, 'data')
-
-# Create the output directory if it doesn't exist
-if not os.path.exists(data_dir):
-    os.makedirs(data_dir)
 
 today = datetime.today().date()
 
 try:
-    subprocess.run([
-        'scrapy', 
-        'runspider', 
-        'abicom/spiders/ppi_crawler.py', 
-        '-O', os.path.join(data_dir, f'{today}_output.json')
-        ])
+    subprocess.run(['scrapy', 'runspider', 'abicom/spiders/ppi_crawler.py', '-O', f'data/{today}_output.json'])
 except subprocess.CalledProcessError as e:
     print(f"Error running Scrapy spider: {e}")
     sys.exit(1)
@@ -32,7 +15,7 @@ except subprocess.CalledProcessError as e:
 options = ['diesel_brl', 'gasolina_brl']
 
 for comb in options:
-    df = wrangle(comb, today, data_dir)
-    gen_graph(df, comb, data_dir)
+    df = wrangle(comb, today)
+    gen_graph(df, comb)
     text = gen_text(df, comb)
-    # create_tweet(text, f"data/{today}_{comb}.jpg")
+    create_tweet(text, f"data/{today}_{comb}.jpg")
